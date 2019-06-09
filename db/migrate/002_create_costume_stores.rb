286ce96a4eb7a6d9a0fcd1311f400835628871bb
Samuel T <ssamt1142@gmail.com>
2019-06-09 01:49:52 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,15 +1,15 @@
 # Create your costume_stores migration here
 
-# class CreateCostumeStores < ActiveRecord::Migration
-#   def change
-#     create_table :costume_stores do |t|
-#       t.string :name
-#       t.string :location
-#       t.integer :number_of_costumes
-#       t.integer :number_of_employees
-#       t.boolean :wheter_or_not_its_still_in_business
-#       t.text :opening_time
-#       t.text :closing_time
-#       t.timestamps
-#   end
-# end
+class CreateCostumeStores < ActiveRecord::Migration
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :number_of_costumes
+      t.integer :number_of_employees
+      t.boolean :wheter_or_not_its_still_in_business
+      t.text :opening_time
+      t.text :closing_time
+      t.timestamps
+  end
+end
